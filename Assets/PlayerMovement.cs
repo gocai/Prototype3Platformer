71ebaa27ff5d7758baed8221ee8aa3e95a45ea6f
prjktalix <briancamilo0720@gmail.com>
2023-12-10 19:16:51 -0800

    cahgne movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,27 +6,23 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 18f;
     public float rotationSpeed = 100f;
     public Transform groundCheck;
-    public float groundCheckRadius = 1.0f;
     public LayerMask groundLayer;
+    
 
     private Rigidbody2D rb;
     private Animator anim;
-    private Camera mainCamera;
     private bool isGrounded;
     private bool isJumping;
-
-    public float gravityScale = 1.0f;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        mainCamera = Camera.main;
     }
 
     private void Update()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundLayer);
 
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
@@ -47,27 +43,10 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
         }
-
-        float zRotation = transform.eulerAngles.z % 360;
-        zRotation = (zRotation + 360) % 360;
-
-        if (zRotation <= 90 || zRotation > 270)
-        {
-            // Normal gravity
-            rb.gravityScale = Mathf.Abs(gravityScale);
-        }
-        else
-        {
-            // Inverted gravity
-            rb.gravityScale = -Mathf.Abs(gravityScale);
-        }
     }
-
-
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
-    }
+        Gizmos.color = Color.red; 
+    }    
 }
